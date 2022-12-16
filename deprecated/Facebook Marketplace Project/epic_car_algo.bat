27a9cc09aac5c448196fe9d8f626790b72de5889
Porter Moody <pmoody@awolff.com>
2022-12-16 10:50:40 -0700

    hello

--- deprecated/Facebook Marketplace Project/epic_car_algo.bat
+++
@@ -1,1 +0,0 @@
-C:/Users/porte/AppData/Local/Microsoft/WindowsApps/python.exe "D:/BYUI/fall 2020/Side Projects/porterbmoody.github.io/Facebook Marketplace Project/scripts/open_browser_and_scrape.py"
